07f3894c07a29db82c40a49a33b81403cb339851
Matthew Carlsen <matthewdcarlsen@gmail.com>
2024-06-05 23:08:54 -0500

    Added Real Work Experience
    - Updated work experience to include actual jobs and descriptions.

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -6,20 +6,28 @@ const Experience = () => {
 
         </div>
 
-        <h3>Animal Colourist <a className="highlight">@ McDonalds</a></h3>
-        <p>
-          Magna fermentum iaculis eu non diam phasellus vestibulum lorem sed. Lacus sed turpis tincidunt id aliquet risus feugiat in ante. Felis eget nunc lobortis mattis aliquam faucibus purus in. Neque vitae tempus quam pellentesque nec. Aliquam sem et tortor consequat. Aliquam nulla facilisi cras fermentum odio. Dolor sit amet consectetur adipiscing elit. Amet consectetur adipiscing elit duis tristique sollicitudin nibh. Malesuada pellentesque elit eget gravida cum sociis natoque. Id consectetur purus ut faucibus. Laoreet non curabitur gravida arcu ac tortor. Dui faucibus in ornare quam viverra orci. In hendrerit gravida rutrum quisque. Donec massa sapien faucibus et molestie ac feugiat sed lectus. Amet purus gravida quis blandit turpis cursus. Enim eu turpis egestas pretium aenean pharetra magna. Est ante in nibh mauris cursus. Volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Iaculis nunc sed augue lacus viverra.
-        </p>
+        <h3>Instructional Assistant <a className="highlight">@ Northcentral Technical College</a></h3>
+        <div>
+          <ul className='jobDetailsList'>
+            <li>Developed and enhanced educational content and instructions for the courses Object-Oriented Programming and Programming Concepts.</li>
+            <li>Analyzed student assignments to ensure proper code functionality and provided feedback to help students resolve faulty code, along with recommendations to improve code quality and consistency.</li>
+            <li>Provided personalized tutoring to help students grasp complex concepts, including design patterns and programming principles, and to improve their debugging skills, code quality, and readability.</li>
+          </ul>
+        </div>
 
-        <h3>Wizard <a className="highlight">@ TechPlace</a></h3>
-        <p>
-          Convallis tellus id interdum velit laoreet. Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt. Fames ac turpis egestas sed tempus urna. In metus vulputate eu scelerisque felis imperdiet proin. Eu augue ut lectus arcu bibendum at varius vel. Sapien eget mi proin sed libero enim sed. Morbi tristique senectus et netus et malesuada fames ac. Massa eget egestas purus viverra accumsan in nisl nisi. Nunc consequat interdum varius sit amet mattis vulputate. Blandit turpis cursus in hac habitasse platea dictumst.
-        </p>
+        <h3>Tutor <a className="highlight">@ Northcentral Technical College</a></h3>
+        <div>
+          <ul className='jobDetailsList'>
+            <li>Assisted students with foundational topics in Data Concepts, Programming Concepts, and Web Design through one-on-one tutoring.</li>
+            <li>Helped students learn to troubleshoot code, understand core principles, and develop practical skills to succeed in their coursework</li>
+          </ul>
+        </div>
 
-        <h3>Digital Overlord <a className="highlight">@ TheInternet</a></h3>
+
+        {/* <h3>Digital Overlord <a className="highlight">@ TheInternet</a></h3>
         <p>
         Convallis tellus id interdum velit laoreet. Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt. Fames ac turpis egestas sed tempus urna. In metus vulputate eu scelerisque felis imperdiet proin. Eu augue ut lectus arcu bibendum at varius vel. Sapien eget mi proin sed libero enim sed. Morbi tristique senectus et netus et malesuada fames ac. Massa eget egestas purus viverra accumsan in nisl nisi. Nunc consequat interdum varius sit amet mattis vulputate. Blandit turpis cursus in hac habitasse platea dictumst.
-        </p>
+        </p> */}
       </section>
       
     );
