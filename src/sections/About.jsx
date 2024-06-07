07e97b66ffd54ca2af5d7efa6bc6257ca3a7e431
Matthew Carlsen <matthewdcarlsen@gmail.com>
2024-06-07 14:02:41 -0500

    Updated About Me Section
    - Added my photo to the about me section
    - added "temporary" styling to get it in the right place.

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,21 +2,29 @@ const About = () => {
     return (
       <section id="about" className='bodySection'>
         <h2 className="heading">About Me</h2>
-        <p>Hello, my name is Matthew Carlsen.  I'm a 21-year-old software developer based in Wausau, Wisconsin.</p>
-        <p>
-          I am currently a student at Northcentral Technical College, where I also work as an Instructional Assistant.  
-          My interest in technology was sparked at a young age with games like Minecraft.  
-          Building block houses led me down the rabbit hole of learning more about computers, eventually assembling my own computer, and now to developing applications for them.  
-          I love to learn new things to feed my curiosity, which keeps me fascinated with innovative technology.  
-          I enjoy seeking out creative solutions to complex problems, and my current passion lies in artificial intelligence.
-        </p>
-        <p>
-          When I'm not at my computer, I love to get outdoors to hike, kayak, and take spontaneous camping trips.  
-          I take delight in picking up and learning about random hobbies, with a recent one being pottery.
-        </p>
+        <div className="gridContainer">
+          <div className="description">
+            <p>Hello, my name is Matthew Carlsen. I'm a 21-year-old software developer based in Wausau, Wisconsin.</p>
+            <p>
+              I am currently a student at Northcentral Technical College, where I also work as an Instructional Assistant.  
+              My interest in technology was sparked at a young age with games like Minecraft.  
+              Building block houses led me down the rabbit hole of learning more about computers, eventually assembling my own computer, and now to developing applications for them.  
+              I love to learn new things to feed my curiosity, which keeps me fascinated with innovative technology.  
+              I enjoy seeking out creative solutions to complex problems, and my current passion lies in artificial intelligence.
+            </p>
+            <p>
+              When I'm not at my computer, I love to get outdoors to hike, kayak, and take spontaneous camping trips.  
+              I take delight in picking up and learning about random hobbies, with a recent one being pottery.
+            </p>
+          </div>
+          <div className="imageContainer">
+            <picture>
+              <img src="public/PortfolioPhotoV2.png" alt="Portfolio Photo"></img>
+            </picture>
+          </div>
+        </div>
       </section>
     );
-
 }
 
 export default About;
